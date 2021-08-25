6709d436da79119d9b87d5314b0d53ea72ab8a45
Dhamodharan31 <85432160+Dhamodharan31@users.noreply.github.com>
2021-08-25 20:27:03 +0530

    Add files via upload

---
+++ sub.js
@@ -0,0 +1,24 @@
+et num1=7
+let num2=3
+sumEl=document.getElementById("sum-el")
+
+function add(){
+    sum=num1+num2
+    sumEl.innerText="SUM="+sum
+
+}
+function subtract(){
+    sum=num1-num2
+    sumEl.innerText="SUM="+sum
+
+}
+function multiply(){
+    sum=num1*num2
+    sumEl.innerText="SUM="+sum
+
+}
+function divide(){
+    sum=num1/num2
+    sumEl.innerText="SUM="+sum
+
+}
